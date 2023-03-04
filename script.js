ffc3a3cee8aecd6c90adefa0eb6a9a20fde71a6e
Ariel Santos <105682233+oaksaint@users.noreply.github.com>
2023-03-03 22:33:19 -0300

    Create functions for basic operations

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 'use strict';
 
-const add = function () {};
+const add = function (number1, number2) {
+  return number1 + number2;
+};
 
-const subtract = function () {};
+const subtract = function (number1, number2) {
+  return number1 - number2;
+};
 
-const multiply = function () {};
+const multiply = function (number1, number2) {
+  return number1 * number2;
+};
 
-const divide = function () {};
+const divide = function (number1, number2) {
+  return number1 / number2;
+};
+
+const operate = function (operator, number1, number2) {
+  if (operator === '+') {
+    return add(number1, number2);
+  } else if (operator === '-') {
+    return subtract(number1, number2);
+  } else if (operator === '*') {
+    return multiply(number1, number2);
+  } else if (operator === '/') {
+    return divide(number1, number2);
+  }
+};
