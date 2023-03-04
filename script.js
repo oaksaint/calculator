9b0b4e613125b2b16e2950fa006dd1d9a77a4584
Ariel Santos <105682233+oaksaint@users.noreply.github.com>
2023-03-04 15:59:16 -0300

    Add basic JS functionality, make the numbers display on the panel

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// Declaration of variables collected from the DOM
+let displayValue = document.querySelector('.display');
+const digits = document.getElementsByClassName('digit-button');
+const operators = document.getElementsByClassName('operator');
+const clearButton = document.querySelector('.clear');
+
+// Declarations of variables that will be used as temporary values
+let firstValue;
+let secondValue;
+let chosenOperator;
+
+// Declaration of basic operation functions
 const add = function (number1, number2) {
   return number1 + number2;
 };
@@ -16,6 +28,7 @@ const divide = function (number1, number2) {
   return number1 / number2;
 };
 
+// Declaration of the equals function
 const operate = function (operator, number1, number2) {
   if (operator === '+') {
     return add(number1, number2);
@@ -27,3 +40,26 @@ const operate = function (operator, number1, number2) {
     return divide(number1, number2);
   }
 };
+
+// Assign the display value to either the first or second value variables
+const getValue = function (value) {
+  if (value === 'firstValue') {
+    firstValue = displayValue.textContent;
+  } else secondValue = displayValue.textContent;
+};
+
+// Make the clicked numbers show up in the display panel
+const showValue = function () {
+  for (let i = 0; i < digits.length; i++) {
+    digits[i].addEventListener('click', function () {
+      displayValue.textContent += digits[i].textContent;
+      // Does not let the numbers overflow the display panel, clear the panel if they surpass the max length of 12
+      if (displayValue.textContent.length > 12) {
+        displayValue.textContent = '';
+      }
+    });
+  }
+};
+
+// Calling the main functions of the application
+showValue();
